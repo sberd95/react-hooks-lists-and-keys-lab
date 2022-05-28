552b6b3cfd8201a3a326264ae219aece3ab611d6
Sergio Berdeja <sergioberdeja@gmail.com>
2022-05-28 11:42:16 -0600

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,9 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{links.map(dest => {
+    return <a key={dest} href={`#${dest}`}>{dest}</a>
+  })}</nav>;
 }
 
 export default NavBar;
